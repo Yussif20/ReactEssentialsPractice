b00bedadfa43d9b1529b7508178c896421f53c93
Yousef <yousefayman2000500@gmail.com>
2023-11-27 09:22:27 +0200

    adding the header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import HeaderImage from "../assets/investment-calculator-logo.png";
+
+const Header = () => {
+  return (
+    <header id="header">
+      <img src={HeaderImage} />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/UserInput.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const UserInput = () => {
+  return (
+    <form id="user-input">
+      <div className="input-group">
+        <div>
+          <label htmlFor="initial investment">Initial Investment</label>
+          <input type="number" />
+        </div>
+        <div>
+          <label htmlFor="initial investment">Initial Investment</label>
+          <input type="number" />
+        </div>
+        <div>
+          <label htmlFor="initial investment">Initial Investment</label>
+          <input type="number" />
+        </div>
+        <div>
+          <label htmlFor="initial investment">Initial Investment</label>
+          <input type="number" />
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default UserInput;
